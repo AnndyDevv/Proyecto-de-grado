7c835b1d8be42b0669be0adb3f2e92ebc078d4b6
&&y Dev <166244696+AnndyDevv@users.noreply.github.com>
2025-12-07 14:21:04 -0500

    arreglos
    arreglos en main y que aprenderas terminado

--- que_aprenderas.js
+++ que_aprenderas.js
@@ -1,4 +1,26 @@
+//boton del menu
+ const btn = document.getElementById('menu-btn');
+        const nav = document.getElementById('nav-menu');
 
+        function checkWidth() {
+            if (window.innerWidth < 700) {
+                btn.style.display = 'block';
+                nav.style.display = 'none';
+            } else {
+                btn.style.display = 'none';
+                nav.style.display = 'flex';
+            }
+        }
+
+        btn.addEventListener('click', () => {
+            nav.style.display = nav.style.display === 'none' ? 'block' : 'none';
+        });
+
+        window.addEventListener('resize', checkWidth);
+        window.addEventListener('load', checkWidth);
+
+
+//
 document.addEventListener("DOMContentLoaded", () => {
             const reveals = document.querySelectorAll(".reveal");
             const observer = new IntersectionObserver((entries) => {
