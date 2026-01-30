789d25ba33a7f1e63876d199adf35d1714f3b1ca
&&y Dev <166244696+AnndyDevv@users.noreply.github.com>
2026-01-30 14:55:15 -0500

    final update

--- competencias_tecnicas.js
+++ competencias_tecnicas.js
@@ -1,20 +1,22 @@
-//boton del menu
- const btn = document.getElementById('menu-btn');
-        const nav = document.getElementById('nav-menu');
 
-        function checkWidth() {
-            if (window.innerWidth < 700) {
-                btn.style.display = 'block';
-                nav.style.display = 'none';
-            } else {
-                btn.style.display = 'none';
-                nav.style.display = 'flex';
-            }
+    window.addEventListener('DOMContentLoaded', () => {
+      const btn = document.getElementById('menu-btn');
+      const nav = document.getElementById('nav-menu');
+
+      function checkWidth() {
+        if (window.innerWidth < 700) {
+          btn.style.display = 'block';
+          nav.classList.remove('active');
+        } else {
+          btn.style.display = 'none';
+          nav.style.display = 'flex';
         }
+      }
 
-        btn.addEventListener('click', () => {
-            nav.style.display = nav.style.display === 'none' ? 'block' : 'none';
-        });
+      btn.addEventListener('click', () => {
+        nav.classList.toggle('active');
+      });
 
-        window.addEventListener('resize', checkWidth);
-        window.addEventListener('load', checkWidth);
+      window.addEventListener('resize', checkWidth);
+      window.addEventListener('load', checkWidth);
+    });
